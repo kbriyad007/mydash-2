56d91d5065a08c60c5964a0b24d45bc0629fff84
kbriyad007 <kb.riyad007@gmail.com>
2025-06-08 12:43:28 +0600

    firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -4,13 +4,15 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth'; // ✅ import auth
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+ // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBj6QysY8iakOIolvgxdVIQFISrkWKLSls",
+  authDomain: "user-data-ff2ef.firebaseapp.com",
+  projectId: "user-data-ff2ef",
+  storageBucket: "user-data-ff2ef.firebasestorage.app",
+  messagingSenderId: "256585563027",
+  appId: "1:256585563027:web:002cbebe818faf9ebec666",
+  measurementId: "G-V3BQPCTJGG"
 };
 
 const app = initializeApp(firebaseConfig);
